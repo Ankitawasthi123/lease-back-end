f06ea3b406c0c537c734f7873110bbe033e26037
Ankitawasthi123 <ankitawasthi86@gmail.com>
2025-04-20 02:02:19 +0530

    check commit

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,6 +4,7 @@ import { registerUser, loginUser } from "../controllers/authController";
 const router = Router();
 
 router.post("/register", registerUser);
+
 router.post("/login", loginUser);
 
 export default router;
